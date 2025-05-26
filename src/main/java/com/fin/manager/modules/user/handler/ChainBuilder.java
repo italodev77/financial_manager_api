dbec34f1267ae4fc761e5c5322b7add48526d3f2
devitalo <italo_ariel_dev77@outlook.com>
2025-05-26 02:05:39 -0300

    feat implementado o Login service e login controller

--- src/main/java/com/fin/manager/modules/user/handler/ChainBuilder.java
+++ src/main/java/com/fin/manager/modules/user/handler/ChainBuilder.java
@@ -1,4 +1,27 @@
 package com.fin.manager.modules.user.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class ChainBuilder {
+
+    private final List<Handler> handlers;
+
+    @Autowired
+    public ChainBuilder(List<Handler> handlers) {
+        this.handlers = handlers;
+    }
+
+    public Handler buildChain() {
+        if (handlers.isEmpty()) {
+            throw new IllegalStateException("Nenhum handler configurado.");
+        }
+        for (int i = 0; i < handlers.size() - 1; i++) {
+            handlers.get(i).setNext(handlers.get(i + 1));
+        }
+        return handlers.get(0);
+    }
 }
---
+++ src/main/java/com/fin/manager/modules/user/service/LoginService.java
@@ -0,0 +1,26 @@
+package com.fin.manager.modules.user.service;
+
+import com.fin.manager.config.TokenService;
+import com.fin.manager.modules.user.dto.LoginContext;
+import com.fin.manager.modules.user.dto.LoginRequestDTO;
+import com.fin.manager.modules.user.handler.ChainBuilder;
+import com.fin.manager.modules.user.handler.Handler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private ChainBuilder chainBuilder;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public String processLogin(LoginRequestDTO loginRequestDTO) {
+        Handler chain = chainBuilder.buildChain();
+        LoginContext context = new LoginContext(loginRequestDTO, null);
+        chain.handle(context);
+        return tokenService.generateToken(context.user());
+    }
+}
